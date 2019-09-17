55c1dff4cb329c072f642d33be22e159c01ea1ad
Andrei Ostanin <aostanin@gmail.com>
2019-09-17 11:53:28 +0900

    Add valmar

---
+++ hosts/valmar/configuration.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+
+let
+  #nixos-hardware = builtins.fetchTarball {
+  #  url = "https://github.com/NixOS/nixos-hardware/archive/master.tar.gz";
+  #};
+in {
+  imports = [
+    #"${nixos-hardware}/apple/macbook-pro/12-1"
+    ./hardware-configuration.nix
+    ../../modules/common
+    ../../modules/desktop
+    ../../modules/syncthing
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking = {
+    hostName = "valmar";
+    hostId = "203d588e";
+    bridges.br0.interfaces = [ "enp0s31f6" ];
+  };
+
+  services.flatpak.enable = true;
+
+  services.openssh.forwardX11 = true;
+
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.extraPools = [ "tank" ];
+  services.zfs.autoScrub.enable = true;
+  services.zfs.autoSnapshot.enable = true;
+
+  virtualisation.libvirtd = {
+    enable = true;
+    qemuVerbatimConfig = ''
+      cgroup_device_acl = [
+        "/dev/null", "/dev/full", "/dev/zero",
+        "/dev/random", "/dev/urandom",
+        "/dev/ptmx", "/dev/kvm", "/dev/kqemu",
+        "/dev/rtc","/dev/hpet", "/dev/sev",
+        "/dev/input/by-id/usb-04d9_USB_Keyboard-event-kbd",
+        "/dev/input/by-id/usb-Logitech_G500s_Laser_Gaming_Mouse_2881723C750008-event-mouse"
+      ]
+    '';
+  };
+
+  boot.kernelModules = [ "vfio_pci" ];
+  boot.kernelParams = [
+    "intel_iommu=on"
+    "vfio-pci.ids=10de:1b81,10de:10f0,1b21:1242"
+    "default_hugepagesz=1G" "hugepagesz=1G" "hugepages=16"
+  ];
+}
