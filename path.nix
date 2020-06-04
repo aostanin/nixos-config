e851fb309dca7af5ffd9fadcfecb885f91abef33
Andrei Ostanin <aostanin@gmail.com>
2020-06-04 10:18:06 +0900

    Downgrade Android Studio, 4.0 is still a bit buggy

--- path.nix
+++ path.nix
@@ -1,6 +1,6 @@
 [
   "nixpkgs=https://github.com/NixOS/nixpkgs/archive/e7752db2fb6c5e05f1de2b275535dbde07d30090.tar.gz"
-  "unstable=https://github.com/NixOS/nixpkgs/archive/1471767b55beb65a206b9725cb96f5af5cd6ccf0.tar.gz"
+  "unstable=https://github.com/NixOS/nixpkgs/archive/4f9053c6050e958205be41a6cd3a71a279ef5fa5.tar.gz"
   "nixos-hardware=https://github.com/NixOS/nixos-hardware/archive/2f2c2d9ef5dcc663a504819ad73cc98a4cd26458.tar.gz"
   "home-manager=https://github.com/rycee/home-manager/archive/96fcf3b017d813fac300ecb821b6db6b7b5d0c40.tar.gz"
 ]
