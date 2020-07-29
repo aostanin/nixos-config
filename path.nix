6d18f906ca5509d79b557a09784307666e92977e
Andrei Ostanin <aostanin@gmail.com>
2020-07-29 09:43:56 +0900

    Update paths

--- path.nix
+++ path.nix
@@ -1,6 +1,6 @@
 [
-  "nixpkgs=https://github.com/NixOS/nixpkgs/archive/bb8f0cc2279934cc2274afb6d0941de30b6187ae.tar.gz"
-  "unstable=https://github.com/NixOS/nixpkgs/archive/8b66a6f863729a9223f31f20adc340113b4ec14a.tar.gz"
-  "nixos-hardware=https://github.com/NixOS/nixos-hardware/archive/cc353d439e3135dbe3f5473d897d2c35537f260c.tar.gz"
+  "nixpkgs=https://github.com/NixOS/nixpkgs/archive/eeb91b03a5cef25c3931bdd4438f006a293adef9.tar.gz"
+  "unstable=https://github.com/NixOS/nixpkgs/archive/56c86e7ce74c3413b98580ef5350d99e21dc265f.tar.gz"
+  "nixos-hardware=https://github.com/NixOS/nixos-hardware/archive/b5a87414c691cc80350d4b87087fadf5b573a278.tar.gz"
   "home-manager=https://github.com/rycee/home-manager/archive/318bc0754ed6370cfcae13183a7f13f7aa4bc73f.tar.gz"
 ]
