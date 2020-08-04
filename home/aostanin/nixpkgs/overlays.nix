b8b9d0054e3b8f79544bb1a79284ba85b0340b11
Andrei Ostanin <aostanin@gmail.com>
2020-08-04 10:07:46 +0900

    Use catt from nixpkgs

--- home/aostanin/nixpkgs/overlays.nix
+++ home/aostanin/nixpkgs/overlays.nix
@@ -4,7 +4,6 @@
       unstable = import <unstable> { };
 
       # TODO: Make this compatible with host systems
-      catt = super.python3Packages.callPackage ../../../packages/catt { };
       crazydiskinfo = super.callPackage ../../../packages/crazydiskinfo { };
       dedbae = super.callPackage ../../../packages/dedbae { };
       hactool = super.callPackage ../../../packages/hactool { };
