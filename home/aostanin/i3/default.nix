133732ca1bac9960ab91da94b476536eb56d1ce1
Andrei Ostanin <aostanin@gmail.com>
2020-09-14 09:58:52 +0900

    Disable pasystray notifications

--- home/aostanin/i3/default.nix
+++ home/aostanin/i3/default.nix
@@ -180,7 +180,7 @@ in
       ${optionalString sysconfig.networking.networkmanager.enable ''
         exec --no-startup-id ${pkgs.networkmanagerapplet}/bin/nm-applet --sm-disable
       ''}
-      exec --no-startup-id ${pkgs.pasystray}/bin/pasystray
+      exec --no-startup-id ${pkgs.pasystray}/bin/pasystray --notify=none
       exec --no-startup-id ${pkgs.barrier}/bin/barrier
       exec --no-startup-id ${pkgs.syncthing-gtk}/bin/syncthing-gtk --minimized
       exec --no-startup-id ${pkgs.flameshot}/bin/flameshot
