88de7c8cfebdf6b28a7a3b96b02d711a47ece604
Andrei Ostanin <aostanin@gmail.com>
2020-10-19 21:06:31 +0900

    Don't bind to vfio-pci but ignore the GPU when not in use instead

--- hosts/valmar/vfio.nix
+++ hosts/valmar/vfio.nix
@@ -25,10 +25,18 @@ in
       "hugepagesz=1G"
       "hugepages=32"
       #"vfio-pci.ids=10de:1b81,10de:10f0" # GTX 1070
-      "vfio-pci.ids=10de:1e84,10de:10f8,10de:1ad8,10de:1ad9" # RTX 2070 Super
+      #"vfio-pci.ids=10de:1e84,10de:10f8,10de:1ad8,10de:1ad9" # RTX 2070 Super
     ];
     extraModprobeConfig = ''
       options kvm-amd nested=1
+    '';
+  };
+
+  services.xserver = {
+    deviceSection = ''
+      # Specify a GPU to avoid using the passthrough GPU
+      BusID "PCI:11:0:0"
+      Option "ProbeAllGpus" "off"
     '';
   };
 
