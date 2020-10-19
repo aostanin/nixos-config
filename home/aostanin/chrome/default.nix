d3c5b7e66830250ac46f4acd0199cc9a16ef8fa3
Andrei Ostanin <aostanin@gmail.com>
2020-10-19 21:05:39 +0900

    Fix Chrome screen corruption after sleep

--- home/aostanin/chrome/default.nix
+++ home/aostanin/chrome/default.nix
@@ -1,9 +1,12 @@
 { pkgs, config, lib, ... }:
 
 {
-  programs.google-chrome.enable = true;
-
-  xdg.configFile."chrome-flags.conf".text = ''
-    --password-store=gnome
-  '';
+  # TODO: Switch to Chromium?
+  programs.google-chrome = {
+    enable = true;
+    package = pkgs.google-chrome.override {
+      # Disable Nvidia screen corruption https://askubuntu.com/a/1275573
+      commandLineArgs = "--use-cmd-decoder=validating --use-gl=desktop";
+    };
+  };
 }
