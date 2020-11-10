b0cecc63bf99394c2196329fff6d5d63213cc730
Andrei Ostanin <aostanin@gmail.com>
2020-11-10 09:53:23 +0900

    Add qemu hooks

--- hosts/valmar/vfio.nix
+++ hosts/valmar/vfio.nix
@@ -13,6 +13,42 @@ let
     wait -n
     pkill -P $$
   '';
+  # https://github.com/PassthroughPOST/VFIO-Tools/blob/master/libvirt_hooks/qemu
+  qemuHook = pkgs.writeShellScript "qemu" ''
+    #!${pkgs.stdenv.shell}
+    #
+    # Author: Sebastiaan Meijer (sebastiaan@passthroughpo.st)
+    #
+    # Copy this file to /etc/libvirt/hooks, make sure it's called "qemu".
+    # After this file is installed, restart libvirt.
+    # From now on, you can easily add per-guest qemu hooks.
+    # Add your hooks in /etc/libvirt/hooks/qemu.d/vm_name/hook_name/state_name.
+    # For a list of available hooks, please refer to https://www.libvirt.org/hooks.html
+    #
+
+    GUEST_NAME="$1"
+    HOOK_NAME="$2"
+    STATE_NAME="$3"
+    MISC="''${@:4}"
+
+    BASEDIR="$(dirname $0)"
+
+    HOOKPATH="$BASEDIR/qemu.d/$GUEST_NAME/$HOOK_NAME/$STATE_NAME"
+
+    set -e # If a script exits with an error, we should as well.
+
+    # check if it's a non-empty executable file
+    if [ -f "$HOOKPATH" ] && [ -s "$HOOKPATH"] && [ -x "$HOOKPATH" ]; then
+        eval \"$HOOKPATH\" "$@"
+    elif [ -d "$HOOKPATH" ]; then
+        while read file; do
+            # check for null string
+            if [ ! -z "$file" ]; then
+              eval \"$file\" "$@"
+            fi
+        done <<< "$(find -L "$HOOKPATH" -maxdepth 1 -type f -executable -print;)"
+    fi
+  '';
 in
 {
   boot = {
@@ -41,9 +77,22 @@ in
     virtmanager
   ];
 
-  systemd.tmpfiles.rules = [
-    "f /dev/shm/looking-glass 0660 aostanin qemu-libvirtd -"
-  ];
+  systemd = {
+    services.libvirtd = {
+      path = with pkgs; [
+        stdenv.shell
+        vfio-isolate
+      ];
+      preStart = ''
+        mkdir -p /var/lib/libvirt/hooks/qemu.d
+        ln -sf ${qemuHook} /var/lib/libvirt/hooks/qemu
+      '';
+    };
+
+    tmpfiles.rules = [
+      "f /dev/shm/looking-glass 0660 aostanin qemu-libvirtd -"
+    ];
+  };
 
   virtualisation.libvirtd = {
     enable = true;
