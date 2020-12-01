c0db795abcfc07b8d9820edce3d0430a8212c2c8
Andrei Ostanin <aostanin@gmail.com>
2020-12-01 19:41:22 +0900

    More 3d printing tools

--- home/aostanin/neovim/default.nix
+++ home/aostanin/neovim/default.nix
@@ -1,7 +1,16 @@
 { pkgs, config, lib, ... }:
-
-with lib;
-
+let
+  vim-openscad = pkgs.vimUtils.buildVimPlugin {
+    pname = "vim-openscad";
+    version = "2020-07-08";
+    src = pkgs.fetchFromGitHub {
+      owner = "sirtaj";
+      repo = "vim-openscad";
+      rev = "81db508b1888fdbea994d43ccef1acd86c8af3f7";
+      sha256 = "1wcdfayjpb9h0lzwdi5nda4c0ch263fdr0379l9k1gf47bgq9cx2";
+    };
+  };
+in
 {
   programs.neovim = {
     enable = true;
@@ -20,8 +29,9 @@ with lib;
       syntastic
       vim-fugitive
       vim-gitgutter
+      vim-openscad
       vim-sensible
     ];
-    extraConfig = readFile ./config;
+    extraConfig = lib.readFile ./config;
   };
 }
