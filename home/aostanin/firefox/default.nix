e4ed85a040ef483c762d807a831a33f3bcfb8da1
Andrei Ostanin <aostanin@gmail.com>
2021-01-18 12:40:17 +0900

    Add Firefox

---
+++ home/aostanin/firefox/default.nix
@@ -0,0 +1,38 @@
+{ pkgs, config, lib, ... }:
+let
+  nur = import <nur> {
+    inherit pkgs;
+  };
+  defaultSettings = {
+    "identity.sync.tokenserver.uri" = "***REMOVED***";
+    "browser.aboutConfig.showWarning" = false;
+    "browser.tabs.warnOnClose" = false;
+  };
+in
+{
+  programs.firefox = {
+    enable = true;
+    profiles = {
+      aostanin = {
+        id = 0;
+        settings = defaultSettings // { };
+      };
+      work = {
+        id = 1;
+        settings = defaultSettings // { };
+      };
+    };
+    extensions = with nur.repos.rycee.firefox-addons; [
+      bitwarden
+      clearurls
+      decentraleyes
+      h264ify
+      https-everywhere
+      i-dont-care-about-cookies
+      multi-account-containers
+      temporary-containers
+      ublock-origin
+      vimium
+    ];
+  };
+}
