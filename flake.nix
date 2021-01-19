f1cb02dca2c80f91359cdb8a8735c68e35d42033
Andrei Ostanin <aostanin@gmail.com>
2021-01-19 21:45:58 +0900

    Initial flake support

--- flake.nix
+++ flake.nix
@@ -3,17 +3,42 @@
 
   inputs = {
     deploy-rs.url = "github:serokell/deploy-rs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nur.url = "github:nix-community/NUR";
     home-manager.url = "github:nix-community/home-manager/release-20.09";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
-    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nur.url = "github:nix-community/NUR";
   };
 
-  outputs = { self, nixpkgs, deploy-rs }: {
-    nixosConfigurations.mareg = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, nixos-hardware, nur, deploy-rs }: {
+    nixosConfigurations.mareg = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
-      modules = [ ./hosts/mareg/configuration.nix ];
+      modules = [
+        {
+          nixpkgs = {
+            config = import ./home/aostanin/nixpkgs/config.nix;
+            overlays = [
+              nur.overlay
+              (final: prev: {
+                unstable = import nixpkgs-unstable {
+                  inherit system;
+                  config = import ./home/aostanin/nixpkgs/config.nix;
+                };
+              })
+            ] ++ (import ./home/aostanin/nixpkgs/overlays.nix);
+          };
+        }
+        nixos-hardware.nixosModules.lenovo-thinkpad-t440p
+        nixos-hardware.nixosModules.common-pc-laptop-ssd
+        ./hosts/mareg/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.root = import ./home/root/home.nix;
+          home-manager.users.aostanin = import ./home/aostanin/home.nix;
+        }
+      ];
     };
 
     deploy.nodes.mareg = {
@@ -23,9 +48,11 @@
       autoRollback = false;
       magicRollback = false;
 
-      profiles.system = {
-        user = "root";
-        path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.mareg;
+      profiles = {
+        system = {
+          user = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.mareg;
+        };
       };
     };
 
