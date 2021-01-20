b6cafb00698342c0f98473045558369017095b60
Andrei Ostanin <aostanin@gmail.com>
2021-01-20 09:24:59 +0900

    Remove unnecessary files

--- shell.nix
+++ shell.nix
@@ -7,15 +7,10 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     cargo # For nixpkgs-fmt
     git-crypt
-    morph
-    niv
     pre-commit
   ];
 
   lorriHook = ''
-    export NIX_PATH="${builtins.concatStringsSep ":" (nixPath ++ [ "." ])}"
-    export NIX_STATE_VERSION="${stateVersion}"
-
     pre-commit install -f --hook-type pre-commit
   '';
 }
