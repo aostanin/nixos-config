748284c333c58a52f4d058d37601ea15d4df128e
Andrei Ostanin <aostanin@gmail.com>
2021-06-04 09:00:50 +0900

    Update to NixOS 21.05

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
 
   inputs = {
     deploy-rs.url = "github:serokell/deploy-rs";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
     home-manager = {
-      url = "github:nix-community/home-manager/release-20.09";
+      url = "github:nix-community/home-manager/release-21.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware";
@@ -37,7 +37,7 @@
                 })
               ] ++ (import ./home/aostanin/nixpkgs/overlays.nix);
             };
-            system.stateVersion = "20.09";
+            system.stateVersion = "21.05";
           }
           (./hosts + "/${hostname}/configuration.nix")
           home-manager.nixosModules.home-manager
