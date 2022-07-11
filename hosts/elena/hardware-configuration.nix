aef7e9fc426dbe40a35a2aa80b67f5089d1156af
Andrei Ostanin <aostanin@gmail.com>
2022-07-12 07:34:01 +0900

    Add manga filesystem

--- hosts/elena/hardware-configuration.nix
+++ hosts/elena/hardware-configuration.nix
@@ -35,6 +35,7 @@ in
     "/storage/media/adult" = zfsFilesystem "tank/media/adult";
     "/storage/media/audiobooks" = zfsFilesystem "tank/media/audiobooks";
     "/storage/media/books" = zfsFilesystem "tank/media/books";
+    "/storage/media/manga" = zfsFilesystem "tank/media/manga";
     "/storage/media/music" = zfsFilesystem "tank/media/music";
     "/storage/media/recorded" = zfsFilesystem "tank/media/recorded";
     "/storage/media/software" = zfsFilesystem "tank/media/software";
