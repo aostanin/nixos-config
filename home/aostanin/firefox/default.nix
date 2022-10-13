807210598ff5aa964e07ee35af54fb0688903feb
Andrei Ostanin <aostanin@gmail.com>
2022-10-13 11:29:56 +0900

    Remove Firefox work profile

--- home/aostanin/firefox/default.nix
+++ home/aostanin/firefox/default.nix
@@ -1,20 +1,6 @@
 { pkgs, config, lib, ... }:
 let
   secrets = import ../../../secrets;
-  defaultSettings = {
-    "browser.aboutConfig.showWarning" = false;
-    "browser.tabs.warnOnClose" = false;
-
-    # Fixed location ref: https://security.stackexchange.com/a/147176
-    "geo.provider.network.url" = "data:application/json,{\"location\": {\"lat\": ${toString secrets.location.coarse.latitude}, \"lng\": ${toString secrets.location.coarse.longitude}}, \"accuracy\": 27000.0}";
-
-    # Privacy
-    "privacy.query_stripping.enabled" = true;
-
-    # Performance
-    "layers.acceleration.force-enabled" = true;
-    "gfx.webrender.all" = true;
-  };
 in
 {
   programs.firefox = {
@@ -22,30 +8,30 @@ in
     profiles = {
       aostanin = {
         id = 0;
-        settings = defaultSettings // { };
-      };
-      work = {
-        id = 1;
-        settings = defaultSettings // { };
+        settings = {
+          "browser.aboutConfig.showWarning" = false;
+          "browser.tabs.warnOnClose" = false;
+
+          # Fixed location ref: https://security.stackexchange.com/a/147176
+          "geo.provider.network.url" = "data:application/json,{\"location\": {\"lat\": ${toString secrets.location.coarse.latitude}, \"lng\": ${toString secrets.location.coarse.longitude}}, \"accuracy\": 27000.0}";
+
+          # Privacy
+          "privacy.query_stripping.enabled" = true;
+
+          # Performance
+          "layers.acceleration.force-enabled" = true;
+          "gfx.webrender.all" = true;
+        };
       };
     };
     extensions = with pkgs.nur.repos.rycee.firefox-addons; [
       bitwarden
-      h264ify
+      cookies-txt
       multi-account-containers
+      simple-tab-groups
       sponsorblock
       ublock-origin
       vimium
     ];
-  };
-
-  xdg.desktopEntries.firefox-work = {
-    name = "Firefox (Work)";
-    genericName = "Web Browser";
-    exec = "firefox -P work %U";
-    icon = "firefox";
-    terminal = false;
-    type = "Application";
-    categories = [ "Network" "WebBrowser" ];
   };
 }
