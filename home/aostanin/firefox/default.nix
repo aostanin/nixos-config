2c1de1ca9d7659cfca6204a8858eb95922237a19
Andrei Ostanin <aostanin@gmail.com>
2022-10-19 09:57:49 +0900

    Update Firefox settings

--- home/aostanin/firefox/default.nix
+++ home/aostanin/firefox/default.nix
@@ -12,9 +12,13 @@ in
           "browser.aboutConfig.showWarning" = false;
           "browser.tabs.warnOnClose" = false;
           "extensions.pocket.enabled" = false;
+          "general.autoScroll" = false;
 
           # Fixed location ref: https://security.stackexchange.com/a/147176
           "geo.provider.network.url" = "data:application/json,{\"location\": {\"lat\": ${toString secrets.location.coarse.latitude}, \"lng\": ${toString secrets.location.coarse.longitude}}, \"accuracy\": 27000.0}";
+
+          # Needed for dark mode for simple-tab-groups
+          "svg.context-properties.content.enabled" = true;
 
           # Privacy
           "privacy.query_stripping.enabled" = true;
@@ -26,11 +30,13 @@ in
       };
     };
     extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      pkgs.nur.repos.rycee.firefox-addons."10ten-ja-reader"
       bitwarden
       cookies-txt
       multi-account-containers
       simple-tab-groups
       sponsorblock
+      statshunters
       temporary-containers
       ublock-origin
       vimium
