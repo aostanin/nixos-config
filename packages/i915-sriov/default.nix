309f02e1084d2046c95e593436b93d85655d3bfc
Andrei Ostanin <aostanin@gmail.com>
2023-02-09 08:03:44 +0900

    Update i915-vfio

--- packages/i915-sriov/default.nix
+++ packages/i915-sriov/default.nix
@@ -11,8 +11,8 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "strongtz";
     repo = "i915-sriov-dkms";
-    rev = "a2385b699636d8394e19fb4078f6d3bd63ee6c0f";
-    sha256 = "sha256-faGJA6YIO/TjyjMwW4x6zSZ4xUR1rnaQepCsM5XSCR0=";
+    rev = "9996e5c11aad87acaea9060a539084f9a5e90952";
+    sha256 = "sha256-TlpaULil4jPX2KPDfw17OUc6mXU+LMVzoO3sj+AjZls=";
   };
 
   setSourceRoot = ''
