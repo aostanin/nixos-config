40fff32fd5b650ae1bf1f4ce92c9be6fc4b73c23
Andrei Ostanin <aostanin@gmail.com>
2023-03-21 20:00:50 +0900

    Update power management

--- hosts/tio/power-management.nix
+++ hosts/tio/power-management.nix
@@ -3,7 +3,16 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  drives = [
+    "/dev/disk/by-id/ata-TOSHIBA_MN07ACA12T_80U0A007FEQG"
+    "/dev/disk/by-id/ata-TOSHIBA_MN07ACA12T_9010A00BFEQG"
+    "/dev/disk/by-id/ata-TOSHIBA_MN07ACA12T_Y240A03MFEQG"
+    "/dev/disk/by-id/ata-TOSHIBA_MN07ACA12T_Y240A07LFEQG"
+    "/dev/disk/by-id/ata-WDC_WD120EMFZ-11A6JA0_9JHA5RKT"
+    "/dev/disk/by-id/ata-WDC_WD120EMFZ-11A6JA0_9RG1G3RC"
+  ];
+in {
   boot.kernelParams = [
     "pcie_aspm.policy=powersave"
   ];
@@ -12,5 +21,20 @@
     powertop.enable = true;
 
     scsiLinkPolicy = "med_power_with_dipm";
+
+    powerUpCommands = ''
+      ${pkgs.hdparm}/bin/hdparm -B 1 -S 60 -y ${lib.concatStringsSep " " drives}
+    '';
+  };
+
+  systemd.services."hd-idle" = {
+    description = "hd-idle - spin down idle hard disks";
+    after = ["suspend.target" "hibernate.target" "hybrid-sleep.target" "suspend-then-hibernate.target"];
+    serviceConfig = {
+      Restart = "on-failure";
+      Type = "simple";
+      ExecStart = "${pkgs.hd-idle}/bin/hd-idle -i 0 ${lib.concatStringsSep " " (map (drive: "-a ${drive} -i 300") drives)}";
+    };
+    wantedBy = ["multi-user.target"];
   };
 }
