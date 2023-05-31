0b8eb482886a65fa161bb51caba01e45ae43bd02
Andrei Ostanin <aostanin@gmail.com>
2023-05-31 14:43:09 +0900

    Remove GitHub token

--- home/aostanin/fish/default.nix
+++ home/aostanin/fish/default.nix
@@ -11,10 +11,6 @@ in {
     enable = true;
     interactiveShellInit = ''
       fish_vi_key_bindings
-
-      ${optionalString pkgs.stdenv.isDarwin ''
-        set -gx HOMEBREW_GITHUB_API_TOKEN=${secrets.githubApiToken}
-      ''}
     '';
   };
 }
--- home/aostanin/zsh/default.nix
+++ home/aostanin/zsh/default.nix
@@ -22,14 +22,10 @@ in {
       source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
       export PATH=$HOME/.local/bin:$PATH
     '';
-    localVariables =
-      {
-        CASE_SENSITIVE = "true";
-        DISABLE_AUTO_UPDATE = "true";
-      }
-      // optionalAttrs pkgs.stdenv.isDarwin {
-        HOMEBREW_GITHUB_API_TOKEN = secrets.githubApiToken;
-      };
+    localVariables = {
+      CASE_SENSITIVE = "true";
+      DISABLE_AUTO_UPDATE = "true";
+    };
     shellAliases = {
       ls = "${pkgs.exa}/bin/exa";
       ll = "ls -l";
