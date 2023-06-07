72025ce2b1553a76bbd7727aa8599209bcba1ac6
Andrei Ostanin <aostanin@gmail.com>
2023-06-07 09:11:35 +0900

    Remove unused

--- hosts/tio/configuration.nix
+++ hosts/tio/configuration.nix
@@ -5,12 +5,6 @@
   ...
 }: let
   secrets = import ../../secrets;
-  drives = [
-    "/dev/disk/by-id/ata-Hitachi_HDS5C3030ALA630_MJ1311YNG052SA"
-    "/dev/disk/by-id/ata-Hitachi_HDS5C3030ALA630_MJ1311YNG2TT6A"
-    "/dev/disk/by-id/ata-Hitachi_HDS5C3030ALA630_MJ1311YNG2TTLA"
-    "/dev/disk/by-id/ata-Hitachi_HDS5C3030ALA630_MJ1311YNG2TV0A"
-  ];
 in {
   imports = [
     "${hardwareModulesPath}/raspberry-pi/4"
@@ -41,47 +35,6 @@ in {
     defaultGateway = secrets.network.home.defaultGateway;
     nameservers = secrets.network.home.nameserversAdguard;
   };
-
-  # services.zrepl = {
-  #   enable = true;
-  #   settings = {
-  #     jobs = [
-  #       {
-  #         type = "pull";
-  #         name = "pull-elena";
-  #         connect = {
-  #           type = "tcp";
-  #           address = "[${secrets.network.zerotier.hosts.elena.address6}]:8889";
-  #         };
-  #         recv = {
-  #           placeholder.encryption = "inherit";
-  #         };
-  #         root_fs = "backup/backup/hosts/zfs/elena";
-  #         interval = "manual";
-  #         pruning = {
-  #           keep_sender = [
-  #             {
-  #               type = "regex";
-  #               regex = ".*";
-  #             }
-  #           ];
-  #           keep_receiver = [
-  #             {
-  #               type = "grid";
-  #               grid = "1x1h(keep=all) | 24x1h | 90x1d";
-  #               regex = "^zrepl_.*";
-  #             }
-  #             {
-  #               type = "regex";
-  #               negate = true;
-  #               regex = "^zrepl_.*";
-  #             }
-  #           ];
-  #         };
-  #       }
-  #     ];
-  #   };
-  # };
 
   environment.systemPackages = with pkgs; [
     libraspberrypi
