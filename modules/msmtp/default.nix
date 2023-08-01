ab87d287365a66f0302d2564d999229c9c9ecc9c
Andrei Ostanin <aostanin@gmail.com>
2023-08-01 10:24:29 +0900

    Fix msmtp credentials

--- modules/msmtp/default.nix
+++ modules/msmtp/default.nix
@@ -11,6 +11,7 @@ in {
       tls = true;
       tls_starttls = true;
       auth = true;
+      from = secrets.email.from;
       host = secrets.email.host;
       port = secrets.email.port;
       user = secrets.email.username;
