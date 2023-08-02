0f1700592a641424584e81c2e5897c71a4065ccc
Andrei Ostanin <aostanin@gmail.com>
2023-08-03 07:30:40 +0900

    Add back mpt3sas

--- hosts/elena/hardware-configuration.nix
+++ hosts/elena/hardware-configuration.nix
@@ -10,7 +10,7 @@
 }: {
   imports = ["${modulesPath}/installer/scan/not-detected.nix"];
 
-  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
+  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "mpt3sas" "usbhid" "usb_storage" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
