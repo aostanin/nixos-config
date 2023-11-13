c661fc45fa738c084a00e813c6b4874c7aa3826c
Andrei Ostanin <aostanin@gmail.com>
2023-11-13 13:02:08 +0900

    Add SwayNC

--- home/aostanin/sway/default.nix
+++ home/aostanin/sway/default.nix
@@ -4,7 +4,10 @@
   lib,
   osConfig,
   ...
-}: {
+}: let
+  # TODO: Need unstable for now https://github.com/NixOS/nixpkgs/pull/251800
+  swayncPkg = pkgs.unstable.swaynotificationcenter;
+in {
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
@@ -44,6 +47,7 @@
           "${modifier}+d" = "exec ${pkgs.rofi}/bin/rofi -show combi";
           "${modifier}+c" = "exec ${rofiWithPlugins}/bin/rofi -show calc -modi calc -no-show-match -no-sort";
           "${modifier}+period" = "exec ${pkgs.rofimoji}/bin/rofimoji";
+          "${modifier}+n" = "exec ${swayncPkg}/bin/swaync-client -t -sw";
           "${modifier}+Shift+s" = "sticky toggle";
           "${modifier}+h" = "focus left";
           "${modifier}+j" = "focus down";
@@ -225,8 +229,8 @@
             #"disk"
             #"memory"
             #"cpu"
-            "backlight"
-            "wireplumber"
+            #"backlight"
+            #"wireplumber"
             "battery"
             "clock"
           ];
@@ -287,4 +291,91 @@
       tray = "always";
     };
   };
+
+  systemd.user = {
+    # TODO: Switch to home-manager module https://github.com/nix-community/home-manager/pull/4249
+    services.swaync = {
+      Unit = {
+        Description = "Swaync notification daemon";
+        PartOf = ["graphical-session.target"];
+        After = ["graphical-session.target"];
+      };
+
+      Service = {
+        Type = "simple";
+        ExecStart = "${swayncPkg}/bin/swaync";
+        Restart = "always";
+      };
+
+      Install = {WantedBy = ["graphical-session.target"];};
+    };
+  };
+
+  xdg.configFile."swaync/config.json".source = pkgs.writeText "swaync/config.json" ''
+    {
+      "$schema": "/etc/xdg/swaync/configSchema.json",
+      "positionX": "right",
+      "positionY": "top",
+      "layer": "overlay",
+      "control-center-layer": "top",
+      "layer-shell": true,
+      "cssPriority": "application",
+      "control-center-margin-top": 0,
+      "control-center-margin-bottom": 0,
+      "control-center-margin-right": 0,
+      "control-center-margin-left": 0,
+      "notification-2fa-action": true,
+      "notification-inline-replies": false,
+      "notification-icon-size": 64,
+      "notification-body-image-height": 100,
+      "notification-body-image-width": 200,
+      "timeout": 10,
+      "timeout-low": 5,
+      "timeout-critical": 0,
+      "fit-to-screen": true,
+      "control-center-width": 500,
+      "control-center-height": 600,
+      "notification-window-width": 500,
+      "keyboard-shortcuts": true,
+      "image-visibility": "when-available",
+      "transition-time": 200,
+      "hide-on-clear": false,
+      "hide-on-action": true,
+      "script-fail-notify": true,
+      "scripts": {
+      },
+      "notification-visibility": {
+      },
+      "widgets": [
+        "inhibitors",
+        "title",
+        "dnd",
+        "mpris",
+        "notifications"
+      ],
+      "widget-config": {
+        "inhibitors": {
+          "text": "Inhibitors",
+          "button-text": "Clear All",
+          "clear-all-button": true
+        },
+        "title": {
+          "text": "Notifications",
+          "clear-all-button": true,
+          "button-text": "Clear All"
+        },
+        "dnd": {
+          "text": "Do Not Disturb"
+        },
+        "label": {
+          "max-lines": 5,
+          "text": "Label Text"
+        },
+        "mpris": {
+          "image-size": 96,
+          "image-radius": 12
+        }
+      }
+    }
+  '';
 }
