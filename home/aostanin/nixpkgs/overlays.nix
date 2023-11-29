eed5d64047fe7419af10ff75e6a7553f4ab3fabf
Andrei Ostanin <aostanin@gmail.com>
2023-11-29 11:14:04 +0900

    Remove unnecessary package overlays

--- home/aostanin/nixpkgs/overlays.nix
+++ home/aostanin/nixpkgs/overlays.nix
@@ -2,12 +2,8 @@
   (
     self: super: {
       # TODO: Make this compatible with host systems
-      crazydiskinfo = super.callPackage ../../../packages/crazydiskinfo {};
       dedbae = super.callPackage ../../../packages/dedbae {};
-      hactool = super.callPackage ../../../packages/hactool {};
-      immersed = super.callPackage ../../../packages/immersed {};
       ninfs = super.python3Packages.callPackage ../../../packages/ninfs {};
-      nsz = super.python3Packages.callPackage ../../../packages/nsz {};
       personal-scripts = super.callPackage ../../../packages/personal-scripts {};
       pidcat = super.callPackage ../../../packages/pidcat {};
       scrutiny = super.callPackage ../../../packages/scrutiny {};
