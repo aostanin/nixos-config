581abcc91cd111ac9ccd75c443b76fcf5daa3998
Andrei Ostanin <aostanin@gmail.com>
2023-12-15 12:02:03 +0900

    Enable fprintd on skye

--- hosts/skye/configuration.nix
+++ hosts/skye/configuration.nix
@@ -119,6 +119,14 @@
   };
 
   services = {
+    fprintd = {
+      enable = true;
+      tod = {
+        enable = true;
+        driver = pkgs.libfprint-2-tod1-goodix;
+      };
+    };
+
     logind.extraConfig = ''
       HandlePowerKey=suspend
     '';
