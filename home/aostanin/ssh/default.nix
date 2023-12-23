a5f5a77d36fc6fe96ab88303bf0ea1f7773e9cbc
Andrei Ostanin <aostanin@gmail.com>
2023-12-24 07:12:57 +0900

    Copy ssh private key

--- home/aostanin/ssh/default.nix
+++ home/aostanin/ssh/default.nix
@@ -15,4 +15,15 @@ in {
     enable = true;
     matchBlocks = ssh_config.matchBlocks;
   };
+
+  # TODO: Remove if home.file allows setting mode: https://github.com/nix-community/home-manager/issues/3090
+  home.activation.copySshPrivateKey = let
+    id_rsa = pkgs.writeText "id_rsa" (builtins.readFile ../../../secrets/ssh/id_rsa);
+    target = config.home.homeDirectory + "/.ssh/id_rsa";
+  in
+    lib.hm.dag.entryAfter ["writeBoundary"] ''
+      $DRY_RUN_CMD rm -f ${target}
+      $DRY_RUN_CMD cp ${id_rsa} ${target}
+      $DRY_RUN_CMD chmod 600 ${target}
+    '';
 }
--- home/root/home.nix
+++ home/root/home.nix
@@ -12,6 +12,17 @@
     # TODO: Nix-ify SSH config
     file.".ssh/config".source = ../../secrets/ssh/ssh_config_root;
 
+    # TODO: Remove if home.file allows setting mode: https://github.com/nix-community/home-manager/issues/3090
+    activation.copySshPrivateKey = let
+      id_rsa = pkgs.writeText "id_rsa" (builtins.readFile ../../secrets/ssh/id_rsa);
+      target = config.home.homeDirectory + "/.ssh/id_rsa";
+    in
+      lib.hm.dag.entryAfter ["writeBoundary"] ''
+        $DRY_RUN_CMD rm -f ${target}
+        $DRY_RUN_CMD cp ${id_rsa} ${target}
+        $DRY_RUN_CMD chmod 600 ${target}
+      '';
+
     file.".docker/config.json".source = secrets.docker."config.json";
   };
 }
