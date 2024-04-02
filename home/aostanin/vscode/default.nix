ad5583c72836ba6b0a296b41d8d1b811d2796761
Andrei Ostanin <aostanin@gmail.com>
2024-04-02 12:46:42 +0900

    Add Elm extension for VSCode

--- home/aostanin/vscode/default.nix
+++ home/aostanin/vscode/default.nix
@@ -11,11 +11,22 @@
       antyos.openscad
       bbenoist.nix
       editorconfig.editorconfig
+      elmtooling.elm-ls-vscode
       jdinhlife.gruvbox
       mkhl.direnv
       ms-azuretools.vscode-docker
       ms-python.python
       vscodevim.vim
+
+      # Used by elm-ls-vscode but not packaged
+      (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+        mktplcRef = {
+          name = "vscode-test-explorer";
+          publisher = "hbenl";
+          version = "2.21.1";
+          sha256 = "sha256-BqKSvSL93o00fksumkoY6WUvrhRqxjkBt2a1XwJIQXA=";
+        };
+      })
     ];
     userSettings = {
       "editor.fontFamily" = "'Hack Nerd Font', 'monospace', monospace, 'Droid Sans Fallback'";
@@ -30,6 +41,12 @@
 
       # Workaround for terminal not working https://github.com/NixOS/nixpkgs/issues/181610
       "terminal.integrated.shellIntegration.enabled" = false;
+
+      # Elm
+      "elmLS.elmPath" = "${pkgs.elmPackages.elm}/bin/elm";
+      "elmLS.elmReviewPath" = "${pkgs.elmPackages.elm-review}/bin/elm-review";
+      "elmLS.elmFormatPath" = "${pkgs.elmPackages.elm-format}/bin/elm-format";
+      "elmLS.elmTestPath" = "${pkgs.elmPackages.elm-test}/bin/elm-test";
     };
   };
 }
