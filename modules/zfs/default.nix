db54bceb6f336d4464f08406c7273b82fa391177
Andrei Ostanin <aostanin@gmail.com>
2024-07-03 11:52:38 -0700

    Remove workaround for zed email

--- modules/zfs/default.nix
+++ modules/zfs/default.nix
@@ -32,15 +32,10 @@ in {
       };
       trim.enable = true;
       zed = {
-        # TODO: Enable once released https://github.com/NixOS/nixpkgs/pull/275028
-        # enableMail = true;
+        enableMail = true;
         settings = {
           ZED_EMAIL_ADDR = secrets.user.emailAddress;
           ZED_NOTIFY_VERBOSE = true;
-
-          # TODO: Remove once enableMail works
-          ZED_EMAIL_PROG = config.security.wrapperDir + "/sendmail";
-          ZED_EMAIL_OPTS = "@ADDRESS@";
         };
       };
     };
