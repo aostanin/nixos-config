283f62f743330ac4fceebfd4d3c2b6688c6cac0f
Andrei Ostanin <aostanin@gmail.com>
2024-08-10 09:16:51 +0900

    Format

--- darwin/flake-module.nix
+++ darwin/flake-module.nix
@@ -13,26 +13,26 @@
     inherit (inputs) nixpkgs nix-darwin;
     inherit (nixpkgs) lib;
   in {
-        darwinConfigurations = let
-          mkDarwinSystem = {
-            hostname,
-            system,
-          }:
-            nix-darwin.lib.darwinSystem {
-              inherit system;
-              specialArgs = {
-                inherit inputs nixpkgsConfig secrets sopsFiles;
-              };
-              modules = [
-                (./hosts + "/${hostname}")
-              ];
-            };
-        in
-          builtins.mapAttrs (hostname: host:
-            mkDarwinSystem {
-              inherit hostname;
-              inherit (host) system;
-            })
-          (lib.filterAttrs (k: v: lib.pathExists (./hosts + "/${k}")) hosts);
+    darwinConfigurations = let
+      mkDarwinSystem = {
+        hostname,
+        system,
+      }:
+        nix-darwin.lib.darwinSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs nixpkgsConfig secrets sopsFiles;
+          };
+          modules = [
+            (./hosts + "/${hostname}")
+          ];
+        };
+    in
+      builtins.mapAttrs (hostname: host:
+        mkDarwinSystem {
+          inherit hostname;
+          inherit (host) system;
+        })
+      (lib.filterAttrs (k: v: lib.pathExists (./hosts + "/${k}")) hosts);
   };
 }
--- home/flake-module.nix
+++ home/flake-module.nix
@@ -13,46 +13,46 @@
     inherit (inputs) nixpkgs home-manager;
     inherit (nixpkgs) lib;
   in {
-        homeConfigurations = let
-          mkHomeConfiguration = {
-            hostname,
-            system,
-          }:
-            home-manager.lib.homeManagerConfiguration rec {
-              pkgs = import nixpkgs ((mkPkgs system) // {inherit system;});
-              modules = let
-                homeDirectory =
-                  if pkgs.stdenv.isDarwin
-                  then "/Users/${secrets.user.username}"
-                  else "/home/${secrets.user.username}";
-              in [
-                ./modules
-                {
-                  home = {
-                    inherit (secrets.user) username;
-                    homeDirectory = homeDirectory;
-                    stateVersion = "24.05";
-                  };
-
-                  sops = {
-                    defaultSopsFile = sopsFiles.default;
-                    age.sshKeyPaths = ["${homeDirectory}/.ssh/id_ed25519"];
-                  };
-                }
-                ./hosts/${hostname}
-                inputs.sops-nix.homeManagerModules.sops
-                inputs.nixvim.homeManagerModules.nixvim
-              ];
-              extraSpecialArgs = {
-                inherit inputs nixpkgsConfig secrets sopsFiles;
+    homeConfigurations = let
+      mkHomeConfiguration = {
+        hostname,
+        system,
+      }:
+        home-manager.lib.homeManagerConfiguration rec {
+          pkgs = import nixpkgs ((mkPkgs system) // {inherit system;});
+          modules = let
+            homeDirectory =
+              if pkgs.stdenv.isDarwin
+              then "/Users/${secrets.user.username}"
+              else "/home/${secrets.user.username}";
+          in [
+            ./modules
+            {
+              home = {
+                inherit (secrets.user) username;
+                homeDirectory = homeDirectory;
+                stateVersion = "24.05";
               };
-            };
-        in
-          builtins.mapAttrs (hostname: host:
-            mkHomeConfiguration {
-              inherit hostname;
-              inherit (host) system;
-            })
-          (lib.filterAttrs (k: v: lib.pathExists (./hosts + "/${k}")) hosts);
+
+              sops = {
+                defaultSopsFile = sopsFiles.default;
+                age.sshKeyPaths = ["${homeDirectory}/.ssh/id_ed25519"];
+              };
+            }
+            ./hosts/${hostname}
+            inputs.sops-nix.homeManagerModules.sops
+            inputs.nixvim.homeManagerModules.nixvim
+          ];
+          extraSpecialArgs = {
+            inherit inputs nixpkgsConfig secrets sopsFiles;
+          };
+        };
+    in
+      builtins.mapAttrs (hostname: host:
+        mkHomeConfiguration {
+          inherit hostname;
+          inherit (host) system;
+        })
+      (lib.filterAttrs (k: v: lib.pathExists (./hosts + "/${k}")) hosts);
   };
 }
