0638d2ba3a36707c3188550c2dbbf62c45a47100
Andrei Ostanin <aostanin@gmail.com>
2024-08-23 05:34:40 +0900

    Use nvidia package from unstable

--- nixos/hosts/elena/default.nix
+++ nixos/hosts/elena/default.nix
@@ -141,14 +141,7 @@
 
   hardware.nvidia = {
     # TODO: Fixes issues with suspend. Remove once 555.58 is availale in stable/unstable.
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "555.58";
-      sha256_64bit = "sha256-bXvcXkg2kQZuCNKRZM5QoTaTjF4l2TtrsKUvyicj5ew=";
-      sha256_aarch64 = "sha256-7XswQwW1iFP4ji5mbRQ6PVEhD4SGWpjUJe1o8zoXYRE=";
-      openSha256 = "sha256-hEAmFISMuXm8tbsrB+WiUcEFuSGRNZ37aKWvf0WJ2/c=";
-      settingsSha256 = "sha256-vWnrXlBCb3K5uVkDFmJDVq51wrCoqgPF03lSjZOuU8M=";
-      persistencedSha256 = "sha256-lyYxDuGDTMdGxX3CaiWUh1IQuQlkI2hPEs5LI20vEVw=";
-    };
+    package = pkgs.unstable.linuxPackages_6_8.nvidia_x11;
     patch.enable = true;
     modesetting.enable = true;
     open = true;
