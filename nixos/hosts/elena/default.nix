f40840e035a11cc2808f25320cb7f5dedf5f9f6f
Andrei Ostanin <aostanin@gmail.com>
2024-09-01 10:52:24 +0900

    Using nvidia from unstable no longer works as kernel versions differ

--- nixos/hosts/elena/default.nix
+++ nixos/hosts/elena/default.nix
@@ -140,8 +140,15 @@
   };
 
   hardware.nvidia = {
-    # TODO: Fixes issues with suspend. Remove once 555.58 is availale in stable/unstable.
-    package = pkgs.unstable.linuxPackages_6_8.nvidia_x11;
+    # TODO: Fixes issues with suspend. Remove once >555.58 is stable.
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "555.58";
+      sha256_64bit = "sha256-bXvcXkg2kQZuCNKRZM5QoTaTjF4l2TtrsKUvyicj5ew=";
+      sha256_aarch64 = "sha256-7XswQwW1iFP4ji5mbRQ6PVEhD4SGWpjUJe1o8zoXYRE=";
+      openSha256 = "sha256-hEAmFISMuXm8tbsrB+WiUcEFuSGRNZ37aKWvf0WJ2/c=";
+      settingsSha256 = "sha256-vWnrXlBCb3K5uVkDFmJDVq51wrCoqgPF03lSjZOuU8M=";
+      persistencedSha256 = "sha256-lyYxDuGDTMdGxX3CaiWUh1IQuQlkI2hPEs5LI20vEVw=";
+    };
     patch.enable = true;
     modesetting.enable = true;
     # TODO: Open has issues with VFIO
