4a6a5e5a820792eb3338acc9fb7373a282f9bb03
Andrei Ostanin <aostanin@gmail.com>
2024-09-29 17:24:11 +0900

    Add sliding sync proxy for matrix

--- nixos/modules/containers/services/matrix.nix
+++ nixos/modules/containers/services/matrix.nix
@@ -11,6 +11,12 @@ in {
   options.localModules.containers.services.${name} = {
     enable = lib.mkEnableOption name;
 
+    enableSlidingSync = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable the Sliding Sync Proxy.";
+    };
+
     enableFacebookBridge = lib.mkOption {
       type = lib.types.bool;
       default = true;
@@ -45,11 +51,18 @@ in {
   config = lib.mkIf cfg.enable {
     sops.secrets = {
       "containers/${name}/postgres_password" = {};
+      "containers/${name}/syncv3_secret" = {};
       "forgejo/registry_token" = {};
     };
 
     sops.templates."synapse-db.env".content = ''
       POSTGRES_PASSWORD=${config.sops.placeholder."containers/${name}/postgres_password"}
+    '';
+
+    sops.templates."sliding-sync.env".content = ''
+      SYNCV3_SECRET=${config.sops.placeholder."containers/${name}/syncv3_secret"}
+      SYNCV3_SERVER=https://${lib.head (config.lib.containers.mkHosts "matrix")}
+      SYNCV3_DB=user=postgres dbname=syncv3 sslmode=disable host=synapse-db password=${config.sops.placeholder."containers/${name}/postgres_password"}
     '';
 
     localModules.containers.containers.synapse = {
@@ -85,12 +98,25 @@ in {
     };
 
     localModules.containers.containers.synapse-db = {
-      raw.image = "docker.io/library/postgres:11-alpine";
+      raw.image = "docker.io/library/postgres:16-alpine";
       networks = [name];
       raw.environmentFiles = [config.sops.templates."synapse-db.env".path];
       volumes.db = {
         parent = name;
         destination = "/var/lib/postgresql/data";
+      };
+    };
+
+    localModules.containers.containers.sliding-sync = lib.mkIf cfg.enableSlidingSync {
+      raw.image = "ghcr.io/matrix-org/sliding-sync:latest";
+      networks = [name];
+      raw.dependsOn = ["synapse-db"];
+      raw.environmentFiles = [config.sops.templates."sliding-sync.env".path];
+      proxy = {
+        enable = true;
+        names = ["matrix-syncv3"];
+        port = 8008;
+        default.enable = true;
       };
     };
 
