33f6a472ef89382dc276161225c2818dfa3308ee
Andrei Ostanin <aostanin@gmail.com>
2025-07-17 07:37:23 +0900

    Remove scrutiny package

--- packages/default.nix
+++ packages/default.nix
@@ -2,7 +2,6 @@
   inhibit-bridge = pkgs.callPackage ./inhibit-bridge {};
   personal-scripts = pkgs.callPackage ./personal-scripts {};
   pidcat = pkgs.callPackage ./pidcat {};
-  scrutiny = pkgs.callPackage ./scrutiny {};
   vfio-isolate = pkgs.python3Packages.callPackage ./vfio-isolate {};
   virtwold = pkgs.callPackage ./virtwold {};
 }
