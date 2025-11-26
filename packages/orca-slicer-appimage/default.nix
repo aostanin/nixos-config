a62213b7cd53c33aca5593fae69d7fc74fcba20a
Andrei Ostanin <aostanin@gmail.com>
2025-11-26 09:30:44 +0900

    Update orca-slicer-appimage

--- packages/orca-slicer-appimage/default.nix
+++ packages/orca-slicer-appimage/default.nix
@@ -1,14 +1,14 @@
 {
   appimageTools,
   fetchurl,
-  webkitgtk_4_0,
+  webkitgtk_4_1,
 }: let
   pname = "orca-slicer-appimage";
-  version = "2.3.0";
+  version = "2.3.1";
 
   src = fetchurl {
-    url = "https://github.com/SoftFever/OrcaSlicer/releases/download/v${version}/OrcaSlicer_Linux_AppImage_V${version}.AppImage";
-    hash = "sha256-cwediOw28GFdt5GdAKom/jAeNIum4FGGKnz8QEAVDAM=";
+    url = "https://github.com/OrcaSlicer/OrcaSlicer/releases/download/v${version}/OrcaSlicer_Linux_AppImage_Ubuntu2404_V${version}.AppImage";
+    hash = "sha256-8ZnlQIkU79u7+k/WdSzWrUcnIJtIi8R7/5oNpfBTpwE=";
   };
 
   appimageContents = appimageTools.extract {
@@ -19,11 +19,12 @@ in
     inherit pname version src;
 
     extraPkgs = pkgs: [
-      webkitgtk_4_0
+      webkitgtk_4_1
     ];
 
     extraInstallCommands = ''
       install -m 444 -D ${appimageContents}/OrcaSlicer.desktop $out/share/applications/OrcaSlicer.desktop
       install -m 444 -D ${appimageContents}/resources/images/OrcaSlicer.svg $out/share/icons/hicolor/scalable/apps/OrcaSlicer.svg
+      substituteInPlace $out/share/applications/OrcaSlicer.desktop --replace-fail "AppRun" "orca-slicer-appimage"
     '';
   }
