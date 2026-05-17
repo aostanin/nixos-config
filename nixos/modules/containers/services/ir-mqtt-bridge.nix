a2024e2eb2469c250917858675cc6eca7804b39b
Andrei Ostanin <aostanin@gmail.com>
2026-05-17 15:12:16 +0900

    Update ir-mqtt-bridge

--- nixos/modules/containers/services/ir-mqtt-bridge.nix
+++ nixos/modules/containers/services/ir-mqtt-bridge.nix
@@ -1,11 +1,16 @@
 {
   lib,
+  pkgs,
   config,
   secrets,
   ...
 }: let
   name = "ir-mqtt-bridge";
   cfg = config.localModules.containers.services.${name};
+
+  devicesFile =
+    (pkgs.formats.toml {}).generate "devices.toml"
+    {device = secrets.irMqttBridge.devices;};
 in {
   options.localModules.containers.services.${name} = {
     enable = lib.mkEnableOption name;
@@ -13,6 +18,11 @@ in {
 
   config = lib.mkIf cfg.enable {
     sops.secrets."forgejo/registry_token" = {};
+    sops.secrets."containers/ir-mqtt-bridge/home_assistant_token" = {};
+
+    sops.templates."${name}.env".content = ''
+      HOME_ASSISTANT_TOKEN=${config.sops.placeholder."containers/ir-mqtt-bridge/home_assistant_token"}
+    '';
 
     localModules.containers.containers.${name} = {
       raw.image = "${secrets.forgejo.registry}/${secrets.forgejo.username}/ir-mqtt:latest";
@@ -20,6 +30,14 @@ in {
         inherit (secrets.forgejo) registry username;
         passwordFile = config.sops.secrets."forgejo/registry_token".path;
       };
+      raw.environment = {
+        RUST_LOG = "debug";
+        MQTT_HOST = secrets.mqtt.host;
+        HOME_ASSISTANT_URL = secrets.homeAssistant.baseUrl;
+        DEVICES_FILE = "/data/devices.toml";
+      };
+      raw.environmentFiles = [config.sops.templates."${name}.env".path];
+      raw.volumes = ["${devicesFile}:/data/devices.toml:ro"];
     };
   };
 }
